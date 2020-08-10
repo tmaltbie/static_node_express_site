922ed58f3d984248d7cba45f1f225068b8e80efc
Trevor Maltbie <60497552+tmaltbie@users.noreply.github.com>
2020-08-09 22:03:25 -0700

    correct interpolation

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const express = require('express')
+const express = require('express');
 const path = require('path');
-
 const app = express();
 
+// access routes
+const indexRouter = require('./routes/index');
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+  // app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // add static middleware
-app.use('/static', express.static('public'))
+app.use('/static', express.static('public'));
 
+// handle post and put requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+// use routes
+app.use('/', indexRouter);
 
-app.listen(3000);
+// host locally
+app.listen(3000, () => {
+  console.log('This is running on localhost 3000')
+});
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { data } = require('../data/data.json');
+const { projects } = require('../data/data.json');
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  console.log(req.params.id)
-  res.render('index', { data });
+router.get('/', (req, res) => {
+  res.render('index', { projects });
 });
 
 /* GET project page */
