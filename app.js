4c6ae7f28d8da44bc417502730315713691fe322
Trevor Maltbie <60497552+tmaltbie@users.noreply.github.com>
2020-08-19 13:31:22 -0700

    updated application logic

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 3000;
 
 // access routes
 const indexRouter = require('./routes/index');
